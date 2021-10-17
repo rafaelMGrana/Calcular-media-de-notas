76757647c464fe25e6a979597c55033ff8b40482
Rafael de Meneses Grana <rafael292@hotmail.com>
2021-10-17 16:55:34 -0300

    Adicionar arquivos de projeto.

---
+++ Calcular média de notas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double nota1, nota2, nota3, media = 0;
+
+            
+            Console.Write("Entre com a nota 1 : ");
+            nota1 = double.Parse(Console.ReadLine());    
+
+            Console.Write("Entre com a nota 2 : ");
+            nota2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Entre com a nota 3 : ");
+            nota3 = double.Parse(Console.ReadLine());
+
+            media = (nota1 + nota2 + nota3) / 3;
+
+            
+            Console.Write("A média ponderada é : " + media);
+
+            Console.ReadKey();   
+        }
+    }
+}
